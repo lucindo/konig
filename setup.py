85c6f54c2e2c74d08bfa43d60fa549a3c79bda2c
Renato Lucindo <lucindo@gmail.com>
2010-11-23 22:02:09 -0200

    skeleton

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: R. Lucindo (lucindo@gmail.com)
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = '0.1'
+
+sdict = {
+    'name' : 'konig',
+    'version' : version,
+    'description' : 'Simple Distributed Graph Database',
+    'long_description' : 'Simple Distributed Graph Database (redis based)',
+    'url': 'http://github.com/lucindo/konig',
+    'download_url' : 'http://cloud.github.com/downloads/lucindo/konig/konig-%s.tar.gz' % version,
+    'author' : 'Renato Lucindo',
+    'author_email' : 'lucindo@gmail.com',
+    'maintainer' : 'Renato Lucindo',
+    'maintainer_email' : 'lucindo@gmail.com',
+    'keywords' : ['Graph', 'Database', 'Distributed', 'Redis'],
+    'license' : 'BOLA',
+    'packages' : ['konig'],
+    'test_suite' : 'test.run_tests',
+    }
+
+setup(**sdict)
---
+++ test/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: R. Lucindo (lucindo@gmail.com)
+
+def run_tests():
+    pass
+
+if __name__ == '__main__':
+    run_tests()
